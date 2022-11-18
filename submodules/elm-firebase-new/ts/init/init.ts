9abe03fe49a49dde088498e8ff0f48b60fbccfe8
pfiadDi <47104478+pfiadDi@users.noreply.github.com>
2022-11-18 11:10:17 +0100

    unne files deleted

--- submodules/elm-firebase-new/ts/init/init.ts
+++
@@ -1,6 +0,0 @@
-import { FirebaseApp, FirebaseOptions, initializeApp } from "firebase/app";
-
-export const initApp = (config : FirebaseOptions) : FirebaseApp => {
-    return initializeApp(config);
-}
-
