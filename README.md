4649da96e6c7a787dddfae2e15578accc93184e9
pfiadDi <47104478+pfiadDi@users.noreply.github.com>
2022-11-18 10:49:36 +0100

    readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # firebasec-callable-function-bug
+
+# Set up
+Run `npm install` in the root directory, as well as in `./functions/` and in `./submodules/elm-firebase-new`.
+Rename the file `firebaseConfig.example.ts` to `firebaseConfig.ts` and add your firebase project api keys there. 
+
+Run `firebase init` to set up hosting and function emulators.
+
+Build with `npm run build` and start it afterwards with `npm start`
---
+++ firebaseConfig.example.ts
@@ -0,0 +1,13 @@
+import { FirebaseOptions } from "firebase/app";
+
+export const firebaseConfig = (host: string): FirebaseOptions => {
+  return {
+    apiKey: "your project api keys"
+    authDomain: "your project api keys"
+    projectId: "your project api keys"
+    storageBucket: "your project api keys"
+    messagingSenderId: "your project api keys"
+    appId: "your project api keys"
+    measurementId: "your project api keys"
+  };
+};
